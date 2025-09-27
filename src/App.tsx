1675409b667aa14914678c32df030d1d8e010235
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-09-27 13:53:47 -0400

    react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import Footbar from './components/Footbar'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Shop from './pages/Shop'
 import './styles/App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
       <Footbar />
     </>
   )
